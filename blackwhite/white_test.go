e993dc66f38615deae1c723f51416a5cc6f2ab32
Cloud <cloud@txthinking.com>
2017-08-10 14:19:34 +0000

    just atest

--- blackwhite/white_test.go
+++ blackwhite/white_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestIsWhite(t *testing.T) {
-	InitWhiteList()
 	ok := IsWhite("211.94.114.48")
 	t.Log(ok)
 
@@ -21,10 +20,16 @@ func TestIsWhite(t *testing.T) {
 
 	ok = IsWhite("121.196.205.97")
 	t.Log(ok)
+
+	ok = IsWhite("a.cn")
+	t.Log(ok)
+
+	ok = IsWhite("txthinking.com")
+	t.Log(ok)
 }
 
 func TestGetWhiteAPP(t *testing.T) {
-	s := string(strings.TrimSpace(was))
+	s := string(strings.TrimSpace(white_app))
 	ss := strings.Split(s, "\n")
 	t.Log(":" + ss[0] + ":")
 	t.Log(":" + ss[len(ss)-1] + ":")
