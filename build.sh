ae2c4fbf74cf1b0a25e3a896f18d2bc1b8f62a42
Cloud <cloud@txthinking.com>
2018-08-28 16:09:50 +0800

    build support prefix, like dev_

--- build.sh
+++ build.sh
@@ -5,48 +5,48 @@ go run pac.go \
     -d https://blackwhite.txthinking.com/white.list \
     -c https://blackwhite.txthinking.com/white_cidr.list \
     -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT' \
-    > white.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- white.pac > _
-mv _ white.pac
+    > ${1}white.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}white.pac > _
+mv _ ${1}white.pac
 
 go run pac.go \
     -m black \
     -d https://blackwhite.txthinking.com/black.list \
     -c https://blackwhite.txthinking.com/black_cidr.list \
     -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT' \
-    > black.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- black.pac > _
-mv _ black.pac
+    > ${1}black.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}black.pac > _
+mv _ ${1}black.pac
 
 go run pac.go \
     -m global \
     -p 'SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT' \
-    > global.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- global.pac > _
-mv _ global.pac
+    > ${1}global.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}global.pac > _
+mv _ ${1}global.pac
 
 go run pac.go \
     -m white \
     -d https://blackwhite.txthinking.com/white.list \
     -c https://blackwhite.txthinking.com/white_cidr.list \
     -p 'PROXY 127.0.0.1:8080; DIRECT' \
-    > http_white.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- http_white.pac > _
-mv _ http_white.pac
+    > ${1}http_white.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}http_white.pac > _
+mv _ ${1}http_white.pac
 
 go run pac.go \
     -m black \
     -d https://blackwhite.txthinking.com/black.list \
     -c https://blackwhite.txthinking.com/black_cidr.list \
     -p 'PROXY 127.0.0.1:8080; DIRECT' \
-    > http_black.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- http_black.pac > _
-mv _ http_black.pac
+    > ${1}http_black.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}http_black.pac > _
+mv _ ${1}http_black.pac
 
 go run pac.go \
     -m global \
     -p 'PROXY 127.0.0.1:8080; DIRECT' \
-    > http_global.pac
-./node_modules/.bin/uglifyjs --compress --mangle -- http_global.pac > _
-mv _ http_global.pac
+    > ${1}http_global.pac
+./node_modules/.bin/uglifyjs --compress --mangle -- ${1}http_global.pac > _
+mv _ ${1}http_global.pac
 
