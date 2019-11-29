80a67bb34e940d7e7a65ca84607de034d67414f0
txthinking <cloud@txthinking.com>
2019-11-29 17:24:44 +0800

    update list

--- removeWhite.sh
+++ removeWhite.sh
@@ -7,7 +7,13 @@ for s
 do
     if [ $(grep -e "^$s$" ./white.list | wc -l) -gt 0 ]
     then
-        sed -r -i "/^$s$/d" ./white.list
+        command -v gsed > /dev/null 2>&1
+        if [ $? == 0 ]
+        then
+            gsed -r -i "/^$s$/d" ./white.list
+        else
+            sed -r -i "/^$s$/d" ./white.list
+        fi
         echo "Removed: $s"
     else
         echo "No: $s"
